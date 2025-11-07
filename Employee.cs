603d889d4dfd34f28ab8ba5a12479b88e9f4343f
austzdee <167780868+austzdee@users.noreply.github.com>
2025-11-07 09:27:50 +0000

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAbstract
+{
+    public class Employee : Person
+    {
+        //implementing the abstract method
+
+        public override string FirstName { get; set; }
+        public override string LastName { get; set; }
+        public override void SayName()
+        {
+            Console.WriteLine("Full Name: " + FirstName + " " + LastName);
+        }
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsoleAbstract
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Creating an instance of Employee object
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+        }
+    }
+}
